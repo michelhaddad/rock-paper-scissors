b1cde5dd2717b61fda55cba77b7d36728d5e5ef7
michelhaddad <michhadd1@gmail.com>
2022-02-27 14:55:25 +0100

    Add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
         extend: {},
+        colors: {
+            ...colors,
+            'main-bg-color': 'radial-gradient(hsl(214, 47%, 23%) to hsl(237, 49%, 15%))',
+        }
     },
     plugins: [],
 }
