fad722a6049be1dd1c2f06dcd09f45810e2ce6b7
michelhaddad <michhadd1@gmail.com>
2022-03-04 20:56:10 +0100

    Update cors policy

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://rock-paper-scissors--backend.herokuapp.com/";
+const URL = "https://rock-paper-scissors--backend.herokuapp.com";
 const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
