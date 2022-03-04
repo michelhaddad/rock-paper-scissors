5be566a307126a2faf129bddce5708ce11f782f9
Michel Haddad <62252495+michelhaddad@users.noreply.github.com>
2022-03-04 22:55:56 +0100

    Stop logging socket events

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,8 +3,8 @@ import { io } from "socket.io-client";
 const URL = "https://rock-paper-scissors--backend.herokuapp.com";
 const socket = io(URL, { autoConnect: false});
 
-socket.onAny((event, ...args) => {
-    console.log(event, args);
-});
+// socket.onAny((event, ...args) => {
+//     console.log(event, args);
+// });
 
 export default socket;
